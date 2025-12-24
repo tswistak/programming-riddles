a9a9d52fe2fe90d2259ba8c1d6cd63fa4179b0d7
Tomasz Swistak <swistak.tomasz@outlook.com>
2025-12-24 22:09:41 +0100

    adventjs 24

---
+++ adventjs2025/day24.js
@@ -0,0 +1,61 @@
+/**
+ * @param {object} tree1 - The first binary tree.
+ * @param {object} tree2 - The second binary tree.
+ * @returns {[boolean, string]}
+ */
+function isTreesSynchronized(tree1, tree2) {
+  const queue = [[tree1, tree2]]
+  while (queue.length) {
+    const [t1, t2] = queue.pop()
+    if (!t1 && !t2) {
+      continue
+    }
+    if ((!t1 && t2) || (t1 && !t2) || t1.value !== t2.value) {
+      return [false, tree1.value]
+    }
+    queue.push([t1.left, t2.right], [t1.right, t2.left])
+  }
+  return [true, tree1.value]
+}
+
+const tree1 = {
+  value: '🎄',
+  left: { value: '⭐' },
+  right: { value: '🎅' }
+}
+
+const tree2 = {
+  value: '🎄',
+  left: { value: '🎅' }
+  right: { value: '⭐' },
+}
+
+isTreesSynchronized(tree1, tree2) // [true, '🎄']
+
+/*
+  tree1          tree2
+   🎄              🎄
+  / \             / \
+⭐   🎅         🎅   ⭐
+*/
+
+const tree3 = {
+  value: '🎄',
+  left: { value: '🎅' },
+  right: { value: '🎁' }
+}
+
+isTreesSynchronized(tree1, tree3) // [false, '🎄']
+
+const tree4 = {
+  value: '🎄',
+  left: { value: '⭐' },
+  right: { value: '🎅' }
+}
+
+isTreesSynchronized(tree1, tree4) // [false, '🎄']
+
+isTreesSynchronized(
+  { value: '🎅' },
+  { value: '🧑‍🎄' }
+) // [false, '🎅']
